1189dc2d0f0fdad1749a7f67b36f51838e2a162b
camp5456 <tcamp@ou.edu>
2015-01-14 14:43:57 -0600

    Added space to Access link
    Added a space between glyphicon and link name "access" on the Database
    a-z list

--- templates/views-bootstrap-accordion-plugin-style--databaselist.tpl.php
+++ templates/views-bootstrap-accordion-plugin-style--databaselist.tpl.php
@@ -4,7 +4,7 @@
 		
 			  <div class="row database_title" style="padding:10px 10px 5px 10px; border-bottom:1px solid #cccccc;">
 					<div class="col-md-10"><h4><?php print $view->style_plugin->get_field($key, "field_link_2"); ?></h4> <p><?php print $view->style_plugin->get_field($key, "field_coverage"); ?></p></div>
-					<div class="col-md-2"><h5><a href="<?php print $view->style_plugin->get_field($key, "field_link_1"); ?>" target="_blank">Access<span class="glyphicon glyphicon-new-window"></span></a></h5></div>
+					<div class="col-md-2"><h5><a href="<?php print $view->style_plugin->get_field($key, "field_link_1"); ?>" target="_blank">Access <span class="glyphicon glyphicon-new-window"></span></a></h5></div>
 			  </div>
 			  <div class="row" style="padding:5px 10px 10px 10px;">
 				<div class="col-md-12"><?php print $view->style_plugin->get_field($key, "field_description"); ?></div>
