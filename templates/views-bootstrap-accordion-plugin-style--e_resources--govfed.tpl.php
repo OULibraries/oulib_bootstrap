e727d020886180ea8343ec3be85a3e1173f80368
camp5456 <tcamp@ou.edu>
2015-07-31 14:25:19 -0500

    template for government docs federal

---
+++ templates/views-bootstrap-accordion-plugin-style--e_resources--govfed.tpl.php
@@ -0,0 +1,38 @@
+<div id="views-bootstrap-accordion-<?php print $id ?>" class="<?php print $classes ?> databaselist">
+  <?php  foreach ($rows as $key => $row):
+            include 'views-ezproxy_url.inc';
+  ?>
+   <div class="panel panel-default">
+		
+			  <div class="row database_title" style="padding:10px 10px 5px 10px; border-bottom:1px solid #cccccc;">
+					<div class="col-md-10"><h4 class="eref"><a href="<?php print $db_url; ?>" target="_blank"><?php print $view->style_plugin->get_field($key, "field_link_1"); ?> <span class="glyphicon glyphicon-new-window"></span></a></h4> <p> <?php print $view->style_plugin->get_field($key, "field_coverage"); ?> </p></div>
+					<div class="col-md-2"><?php print $view->style_plugin->get_field($key, "ops"); ?></div>
+			  </div>
+			  <div class="row" style="padding:5px 10px 10px 10px;">
+				<div class="col-md-12"><?php print $view->style_plugin->get_field($key, "field_description"); ?></div>
+				<div class="col-md-12"><div class="resourcetype"><?php print $view->style_plugin->get_field($key, "field_resourcetype"); ?></div></div>
+				</div>
+		
+     <!-- <div class="panel-heading">
+        <h4 class="panel-title">
+		     <a class="accordion-toggle"
+             data-toggle="collapse"
+             data-parent="#views-bootstrap-accordion-<?php print $id ?>"
+             href="#collapse<?php print $key ?>">
+            <span class="caret"></span> <?php print $titles[$key] ?>
+          </a>
+          
+        </h4>
+      </div> -->
+
+      <div id="collapse<?php print $key ?>" class="panel-collapse collapse">
+        <div class="panel-body">
+          <?php print $row ?><br>
+		  <Strong>Related Subjects:</strong> <?php print $view->style_plugin->get_field($key, "field_subjects"); ?><br>
+		  <!-- <strong>Permalink:</strong> <?php print $view->style_plugin->get_field($key, "field_link_2"); ?> -->
+		  
+        </div>
+      </div>
+    </div>
+  <?php endforeach ?>
+</div>
