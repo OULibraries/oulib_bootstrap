1de788ecf52b2b00a2352355b82c13fca1e9c1a9
camp5456 <tcamp@ou.edu>
2015-12-10 13:38:27 -0600

    Taking drop down on More Info
    on the single node view

--- templates/views-bootstrap-accordion-plugin-style--e_resources--individual.tpl.php
+++ templates/views-bootstrap-accordion-plugin-style--e_resources--individual.tpl.php
@@ -25,23 +25,15 @@
 					<hr>
 					<h4>More Information</h4>
 					<p><?php print $view->style_plugin->get_field($key, "field_info"); ?></p> 
+					<p><Strong>Related Subjects:</strong> <?php print $view->style_plugin->get_field($key, "field_subjects"); ?></p>
+					<p><strong>Permalink:</strong> <?php print $db_url; ?><br /></p>
+					<p><?php print $view->style_plugin->get_field($key, "field_endnote"); ?></p>
 				</div>
-				
-				<div class="col-md-12"><div class="resourcetype"><?php print $view->style_plugin->get_field($key, "field_resourcetype"); ?>
-				</div></div>
+				<div class="col-md-12">
+					<div class="resourcetype"><?php print $view->style_plugin->get_field($key, "field_resourcetype"); ?></div>
 				</div>
-		
+				</div>
       
-
-      <div id="collapse<?php print $key ?>" class="panel-collapse collapse">
-        <div class="panel-body">
-          <?php print $view->style_plugin->get_field($key, "field_info"); ?><br /><br />
-		  <Strong>Related Subjects:</strong> <?php print $view->style_plugin->get_field($key, "field_subjects"); ?><br />
-		  <strong>Permalink:</strong> <?php print $db_url; ?><br />
-		  <?php print $view->style_plugin->get_field($key, "field_endnote"); ?>
-		  
-        </div>
-      </div>
     </div>
   <?php endforeach ?>
 </div>
