2eaf0e68529c3b437903fff1c6dc7685ee900dd7
c-corbett <ccorbett@ou.edu>
2016-03-18 10:50:54 -0500

    Second Paragraphs Commit
    Style & PHP templates

---
+++ templates/paragraphs-item--image_right_text_left.tpl.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for a single paragraph item.
+ *
+ * Available variables:
+ * - $content: An array of content items. Use render($content) to print them
+ *   all, or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - entity
+ *   - entity-paragraphs-item
+ *   - paragraphs-item-{bundle}
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened into
+ *   a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_entity()
+ * @see template_process()
+ */
+?>
+<div class="container-fluid">
+<div class="paragraph_bundle_image_right row">
+	
+	<div class="col-md-8">
+	<?php print render($content['field_description']); ?>
+	</div>
+	
+	<div class="paragraphs_image col-md-4">
+	<?php print render($content['field_image']); ?>
+	</div>
+</div>
+
+
+
+
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print render($content); ?>
+	
+  </div>
+</div>
+</div>
