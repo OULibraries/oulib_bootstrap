e698502ae78c42cdba345cc626b13d27f923e235
Twila Camp <tcamp@ou.edu>
2017-01-31 10:33:44 -0600

    removing old chat client code

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -345,15 +345,6 @@
 
 <!-- footer -->
 </div>
-<!-- Place this script as near to the end of your BODY as possible. -->
-<script 
- type="text/javascript"> 
-  (function() {
-    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?multi,poll";
-    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-  })();
-</script>
 
 
 <script type="text/javascript">
