44d8a9e573bb6377a1190a49093fd055b90227b2
Twila Camp <tcamp@ou.edu>
2017-04-17 21:46:42 -0500

    adding site name to branding

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -137,8 +137,15 @@
 				<div class="navbar-header">
 					 
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+						 <span class="sr-only">Toggle navigation</span>
+						 <span class="icon-bar"></span>
+						 <span class="icon-bar"></span>
+						 <span class="icon-bar"></span>
 					</button> 
+					
+					<?php if (!empty($page['site-name'])): ?>
+                        <?php print render($page['site-name']); ?>
+                      <?php endif; ?>
 					
 				</div> <!-- /.navbar-header -->
 				
