5f37ea34e6283905a107fb15ade03479b2a2b25b
camp5456 <twilacamp@gmail.com>
2017-09-27 14:14:10 -0500

    adding alt information to footer links

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -303,15 +303,15 @@
                         <ul>
 
                             <li><a href="/contact"
-                                   title="Contact Us">Contact Us</a></li>
+                                   alt="Contact Us link">Contact Us</a></li>
 
                             <li><a href="/aboutsite" title="About This Site">About This Site</a></li>
 
                             <li><a href="http://www.ou.edu/publicaffairs/WebPolicies/accessstatement.html"
-                                   title="Accessibility" target="_blank">Accessibility</a></li>
+                                   alt="Accessibility link" target="_blank">Accessibility</a></li>
 							
 							<li><a href="https://libraries.ou.edu/jobs"
-                                   title="Jobs at OU Libraries" target="_blank">Job Opportunities</a></li>
+                                   alt="Jobs link" target="_blank">Job Opportunities</a></li>
 
                         </ul>
 
@@ -324,17 +324,17 @@
 
                             <li>
                                 <a href="http://www.ou.edu/content/publicaffairs/WebPolicies/copyright.html"
-                                   title="Copyright"
+                                   alt="Copyright link"
                                    target="_blank">Copyright</a></li>
 
                             <li>
                                 <a href="http://www.ou.edu/content/web/landing/policy.html"
-                                   title="Policies" target="_blank">Policies</a>
+                                   alt="Policies link" target="_blank">Policies</a>
                             </li>
 
                             <li>
                                 <a href="http://www.ou.edu/content/web/landing/legalnotices.html"
-                                   title="Legal Notice" target="_blank">Legal
+                                   alt="Legal Notice link" target="_blank">Legal
                                     Notice</a></li>
 
                         </ul>
