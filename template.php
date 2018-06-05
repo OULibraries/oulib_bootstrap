f03e3fcf9cbb2a8a4f8f7d8da95ff8285348f1e3
c-corbett <c-corbett@users.noreply.github.com>
2018-06-05 16:12:22 -0500

    Reverting

--- template.php
+++ template.php
@@ -25,8 +25,3 @@ function oulib_bootstrap_preprocess_html (&$vars) {
   drupal_add_library('system', 'ui');
    
  }
-
-function oulib_bootstrap_admin_paths_alter(&$paths) {
-    // Treat all user pages as not administrative.
-    $paths['user/*'] = FALSE;
-}
